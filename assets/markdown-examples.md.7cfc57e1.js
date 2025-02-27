3dc7af146136052e05d8e6706783b079ad87244a
zuishuaicc <zuishuaicc@users.noreply.github.com>
2025-02-27 04:50:48 +0000

    deploy: 52979f521f006dba2b94393391fb3426ebe915ce

---
+++ assets/markdown-examples.md.7cfc57e1.js
@@ -0,0 +1,65 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.b85f4cea.js";const m=JSON.parse('{"title":"Markdown Extension Examples","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"markdown-examples.md","filePath":"markdown-examples.md","lastUpdated":1740631806000}'),p={name:"markdown-examples.md"},e=l(`<h1 id="markdown-extension-examples" tabindex="-1">Markdown Extension Examples <a class="header-anchor" href="#markdown-extension-examples" aria-label="Permalink to &quot;Markdown Extension Examples&quot;">​</a></h1><p>This page demonstrates some of the built-in markdown extensions provided by VitePress.</p><h2 id="syntax-highlighting" tabindex="-1">Syntax Highlighting <a class="header-anchor" href="#syntax-highlighting" aria-label="Permalink to &quot;Syntax Highlighting&quot;">​</a></h2><p>VitePress provides Syntax Highlighting powered by <a href="https://github.com/shikijs/shiki" target="_blank" rel="noreferrer">Shiki</a>, with additional features like line-highlighting:</p><p><strong>Input</strong></p><div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight vp-code-dark"><code><span class="line"><span style="color:#babed8;">\`\`\`js{4}</span></span>
+<span class="line"><span style="color:#babed8;">export default {</span></span>
+<span class="line"><span style="color:#babed8;">  data () {</span></span>
+<span class="line"><span style="color:#babed8;">    return {</span></span>
+<span class="line"><span style="color:#babed8;">      msg: &#39;Highlighted!&#39;</span></span>
+<span class="line"><span style="color:#babed8;">    }</span></span>
+<span class="line"><span style="color:#babed8;">  }</span></span>
+<span class="line"><span style="color:#babed8;">}</span></span>
+<span class="line"><span style="color:#babed8;">\`\`\`</span></span></code></pre><pre class="shiki material-theme-palenight vp-code-light"><code><span class="line"><span style="color:#babed8;">\`\`\`js{4}</span></span>
+<span class="line"><span style="color:#babed8;">export default {</span></span>
+<span class="line"><span style="color:#babed8;">  data () {</span></span>
+<span class="line"><span style="color:#babed8;">    return {</span></span>
+<span class="line"><span style="color:#babed8;">      msg: &#39;Highlighted!&#39;</span></span>
+<span class="line"><span style="color:#babed8;">    }</span></span>
+<span class="line"><span style="color:#babed8;">  }</span></span>
+<span class="line"><span style="color:#babed8;">}</span></span>
+<span class="line"><span style="color:#babed8;">\`\`\`</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br></div></div><p><strong>Output</strong></p><div class="language-js vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki material-theme-palenight has-highlighted-lines vp-code-dark"><code><span class="line"><span style="color:#89DDFF;font-style:italic;">export</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;font-style:italic;">default</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">  </span><span style="color:#F07178;">data</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">()</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;font-style:italic;">return</span><span style="color:#F07178;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line highlighted"><span style="color:#F07178;">      msg</span><span style="color:#89DDFF;">:</span><span style="color:#F07178;"> </span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Highlighted!</span><span style="color:#89DDFF;">&#39;</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span></code></pre><pre class="shiki material-theme-palenight has-highlighted-lines vp-code-light"><code><span class="line"><span style="color:#89DDFF;font-style:italic;">export</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;font-style:italic;">default</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">  </span><span style="color:#F07178;">data</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">()</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;font-style:italic;">return</span><span style="color:#F07178;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line highlighted"><span style="color:#F07178;">      msg</span><span style="color:#89DDFF;">:</span><span style="color:#F07178;"> </span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Highlighted!</span><span style="color:#89DDFF;">&#39;</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br></div></div><h2 id="custom-containers" tabindex="-1">Custom Containers <a class="header-anchor" href="#custom-containers" aria-label="Permalink to &quot;Custom Containers&quot;">​</a></h2><p><strong>Input</strong></p><div class="language-md vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki material-theme-palenight vp-code-dark"><code><span class="line"><span style="color:#BABED8;">::: info</span></span>
+<span class="line"><span style="color:#BABED8;">This is an info box.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: tip</span></span>
+<span class="line"><span style="color:#BABED8;">This is a tip.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: warning</span></span>
+<span class="line"><span style="color:#BABED8;">This is a warning.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: danger</span></span>
+<span class="line"><span style="color:#BABED8;">This is a dangerous warning.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: details</span></span>
+<span class="line"><span style="color:#BABED8;">This is a details block.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span></code></pre><pre class="shiki material-theme-palenight vp-code-light"><code><span class="line"><span style="color:#BABED8;">::: info</span></span>
+<span class="line"><span style="color:#BABED8;">This is an info box.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: tip</span></span>
+<span class="line"><span style="color:#BABED8;">This is a tip.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: warning</span></span>
+<span class="line"><span style="color:#BABED8;">This is a warning.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: danger</span></span>
+<span class="line"><span style="color:#BABED8;">This is a dangerous warning.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">::: details</span></span>
+<span class="line"><span style="color:#BABED8;">This is a details block.</span></span>
+<span class="line"><span style="color:#BABED8;">:::</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br></div></div><p><strong>Output</strong></p><div class="info custom-block"><p class="custom-block-title">INFO</p><p>This is an info box.</p></div><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip.</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning.</p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>This is a dangerous warning.</p></div><details class="details custom-block"><summary>Details</summary><p>This is a details block.</p></details><h2 id="more" tabindex="-1">More <a class="header-anchor" href="#more" aria-label="Permalink to &quot;More&quot;">​</a></h2><p>Check out the documentation for the <a href="https://vitepress.dev/guide/markdown" target="_blank" rel="noreferrer">full list of markdown extensions</a>.</p>`,19),o=[e];function i(t,r,c,d,b,y){return n(),a("div",null,o)}const u=s(p,[["render",i]]);export{m as __pageData,u as default};
